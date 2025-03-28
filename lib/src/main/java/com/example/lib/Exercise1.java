7db17e46006c5b7b0cde7d5aa0632e5b6d3e6468
sharafadeen hammed <sharafadeenhammed@yahoo.com>
2025-03-28 19:54:03 +0100

    claculate circle area and perimeter from user input

--- lib/src/main/java/com/example/lib/Exercise1.java
+++ lib/src/main/java/com/example/lib/Exercise1.java
@@ -6,7 +6,8 @@ public class Exercise1 {
 
 
     public static void main (String[] args){
-        inputDivisionAndRemainder();
+        //inputDivisionAndRemainder();
+        circleAreaAndPerimeter();
     }
 
     static void inputDivisionAndRemainder (){
@@ -19,5 +20,15 @@ public class Exercise1 {
         int remainder = n1%n2;
         System.out.println("n1/m2 = "+division);
         System.out.println("remainder of n1/n2 = "+ remainder);
+    }
+
+    static void circleAreaAndPerimeter(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter the circle radius...\n");
+        int radius = in.nextInt();
+        double perimeter = (double) 2 * Math.PI * radius;
+        double area = (double) Math.PI * radius * radius;
+        System.out.println("area of circle = " + area);
+        System.out.println("perimeter of circle = " + perimeter );
     }
 }
