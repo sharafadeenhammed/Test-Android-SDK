e88bc069157293efc711360c826155b0466e0632
sharafadeen hammed <sharafadeenhammed@yahoo.com>
2025-03-31 13:12:16 +0100

    basic ui components

--- app/src/main/java/com/example/javaexamples/MainActivity.java
+++ app/src/main/java/com/example/javaexamples/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.javaexamples;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+//import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // get text entered by user...
+        //EditText textInput = findViewById(R.id.editText);
+        //textInput.setText("hello friends how are you doing today...");
+        //String textEntered = textInput.getText().toString();
+
+        Button myButton = findViewById(R.id.myButton);
+        myButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You clicked!!", Toast.LENGTH_LONG ).show();
+            }
+        });
 
     }
 }
