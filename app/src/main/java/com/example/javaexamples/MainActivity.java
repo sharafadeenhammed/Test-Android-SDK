4e96f791c291ee5404342953f654261f14c88e14
sharafadeen hammed <sharafadeenhammed@yahoo.com>
2025-04-04 18:25:50 +0100

    fixed constait view

--- app/src/main/java/com/example/javaexamples/MainActivity.java
+++ app/src/main/java/com/example/javaexamples/MainActivity.java
@@ -14,27 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // get text entered by user...
-        Button myButton = findViewById(R.id.myButton);
-        myButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                updateValues();
-            }
-        });
     }
 
-    void updateValues (){
-        TextView poundTextView = findViewById(R.id.weightInPounds);
-        EditText poundEditTextView = findViewById(R.id.weightInKilo);
-        final String  enteredValue = poundEditTextView.getText().toString();
-        final double kilos =  enteredValue.isEmpty() ? 0 : Double.parseDouble(enteredValue);
-        final double valueInPound = convertKiloToPounds(kilos);
-        poundTextView.setText(valueInPound > 0 ? valueInPound+" pounds": "");
-    }
-
-    double convertKiloToPounds(double kilos){
-        // 1 kilo = 2.20462 pounds
-        return kilos * 2.20462;
-    }
 }
