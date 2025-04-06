f057b99fc9c76ede176a02138fc9c1f5c6c259fc
sharafadeen hammed <sharafadeenhammed@yahoo.com>
2025-04-06 16:58:18 +0100

    completed navigation by explicit intent by changing activity

--- app/src/main/java/com/example/javaexamples/MainActivity.java
+++ app/src/main/java/com/example/javaexamples/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.javaexamples;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "ON CREATE TOAST MESSAGE", Toast.LENGTH_LONG).show();
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoSecondActivity();
+            }
+        });
+    }
+
+    void gotoSecondActivity(){
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 
     @Override
